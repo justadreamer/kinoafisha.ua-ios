1423acec9d091ac39834b3b7e525c9269ee92b4f
Eugene Dorfman <eugene.dorfman@gmail.com>
2015-09-13 15:05:12 +0200

    pod install HockeySDK

--- Pods/Target Support Files/Pods-kinoafisha/Pods-kinoafisha-resources.sh
+++ Pods/Target Support Files/Pods-kinoafisha/Pods-kinoafisha-resources.sh
@@ -59,9 +59,11 @@ install_resource()
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_resource "SVProgressHUD/SVProgressHUD/SVProgressHUD.bundle"
+  install_resource "${BUILT_PRODUCTS_DIR}/HockeySDKResources.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_resource "SVProgressHUD/SVProgressHUD/SVProgressHUD.bundle"
+  install_resource "${BUILT_PRODUCTS_DIR}/HockeySDKResources.bundle"
 fi
 
 mkdir -p "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
