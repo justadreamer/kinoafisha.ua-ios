d4a357d6501e503ee4b375536e4d9761ecbd2e39
Eugene Dorfman <eugene.dorfman@gmail.com>
2015-09-15 08:15:02 +0200

    pod install Flurry SDK

--- Pods/Target Support Files/Pods-kinoafisha/Pods-kinoafisha-acknowledgements.markdown
+++ Pods/Target Support Files/Pods-kinoafisha/Pods-kinoafisha-acknowledgements.markdown
@@ -289,6 +289,10 @@ THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
    limitations under the License.
 
 
+## Flurry-iOS-SDK
+
+Developer's use of the SDK is governed by the license in the applicable Flurry Terms of Service. Some components of the SDK are governed by open source software licenses. In the event of any conflict between the license in the applicable Flurry Terms of Service and the applicable open source license, the terms of the open source license shall prevail with respect to those components.
+
 ## HockeySDK
 
 ## Licenses
--- Pods/Target Support Files/Pods-kinoafisha/Pods-kinoafisha-resources.sh
+++ Pods/Target Support Files/Pods-kinoafisha/Pods-kinoafisha-resources.sh
@@ -59,10 +59,17 @@ install_resource()
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_resource "SVProgressHUD/SVProgressHUD/SVProgressHUD.bundle"
+  install_resource "${BUILT_PRODUCTS_DIR}/Flurry-iOS-SDK.bundle"
   install_resource "${BUILT_PRODUCTS_DIR}/HockeySDKResources.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_resource "SVProgressHUD/SVProgressHUD/SVProgressHUD.bundle"
+  install_resource "${BUILT_PRODUCTS_DIR}/Flurry-iOS-SDK.bundle"
+  install_resource "${BUILT_PRODUCTS_DIR}/HockeySDKResources.bundle"
+fi
+if [[ "$CONFIGURATION" == "AppStore" ]]; then
+  install_resource "SVProgressHUD/SVProgressHUD/SVProgressHUD.bundle"
+  install_resource "${BUILT_PRODUCTS_DIR}/Flurry-iOS-SDK.bundle"
   install_resource "${BUILT_PRODUCTS_DIR}/HockeySDKResources.bundle"
 fi
 
