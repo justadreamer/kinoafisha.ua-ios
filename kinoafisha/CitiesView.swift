213a26b3bd078db722a088a8f65b1e4b41aa26e5
Eugene Dorfman <eugene.dorfman@gmail.com>
2019-07-04 22:07:44 +0200

    wip implementing swiftui

---
+++ kinoafisha/CitiesView.swift
@@ -0,0 +1,23 @@
+//
+//  CitiesView.swift
+//  kinoafisha
+//
+//  Created by eugene on 02.07.2019.
+//  Copyright © 2019 justadreamer. All rights reserved.
+//
+
+import SwiftUI
+
+struct CitiesView : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct CitiesView_Previews : PreviewProvider {
+    static var previews: some View {
+        CitiesView()
+    }
+}
+#endif
