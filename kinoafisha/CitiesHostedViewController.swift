5232acc061f8b0af281784e37247d6e00dde73db
Eugene Dorfman <eugene.dorfman@gmail.com>
2019-07-06 18:44:40 +0200

    wip adding ContainerViewController

---
+++ kinoafisha/CitiesHostedViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CitiesHostedViewController.swift
+//  kinoafisha
+//
+//  Created by Eugene Dorfman on 6/7/19.
+//  Copyright © 2019 justadreamer. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+class CitiesHostedViewController: ContainerViewController {
+    override func viewDidLoad() {
+        let hosting = UIHostingController(rootView: CitiesView())
+        self.content = hosting
+        super.viewDidLoad()
+    }
+}
---
+++ kinoafisha/ContainerViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ContainerViewController.swift
+//  kinoafisha
+//
+//  Created by eugene on 04.07.2019.
+//  Copyright © 2019 justadreamer. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+@objc class ContainerViewController: UIViewController {
+    var content: UIViewController?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+       /* if let content = content {
+            display(content: content)
+        }*/
+    }
+    
+    func display(content: UIViewController) {
+        addChild(content)
+        content.view.frame = view.bounds
+        content.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(content.view)
+        content.didMove(toParent: self)
+    }
+}
