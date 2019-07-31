f0148b584411bff1e4061ec924ebbc58e79ffb65
Eugene Dorfman <eugene.dorfman@gmail.com>
2019-07-31 09:36:21 +0200

    added AWS keys of the readonly user as part of S3Secrets.h
    removed S3 keys

---
+++ kinoafisha/S3Secrets.h
@@ -0,0 +1,16 @@
+//
+//  S3Secrets.h
+//  kinoafisha
+//
+//  Created by Eugene Dorfman on 1/7/15.
+//  Copyright (c) 2015 justadreamer. All rights reserved.
+//
+
+#ifndef kinoafisha_S3Secrets_h
+#define kinoafisha_S3Secrets_h
+
+#define S3AccessKey @""
+#define S3SecretKey @""
+#define S3BucketName @"kinoafisha"
+
+#endif
