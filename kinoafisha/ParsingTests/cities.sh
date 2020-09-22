ae0aede03deddb5817a955eb51e37abc6a70bc42
Eugene Dorfman <eugene.dorfman@gmail.com>
2020-09-22 23:43:57 +0300

    snapshot testing cities parser output

--- kinoafisha/ParsingTests/cities.sh
+++ kinoafisha/ParsingTests/cities.sh
@@ -1,1 +1,2 @@
-~/bin/xsltproc --html --param 'baseURL' '"https://kinoafisha.com"' ../XSLT/cities.xsl ../html/cities.html | jsonlint
+~/bin/xsltproc --html --param 'baseURL' '"https://kinoafisha.com"' ../XSLT/cities.xsl ../html/cities.html 2>/dev/null | jsonlint > cities.json.gitignore
+diff cities.json.gitignore cities_snapshot.json
